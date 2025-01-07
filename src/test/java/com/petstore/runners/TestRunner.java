faea27dd5ff1e3851d75ff3f7d422b86b133cfbb
gaston genaud <gastongenaudar@gmail.com>
2025-01-07 18:04:52 -0300

    TestRunner implementation

--- src/test/java/com/petstore/runners/TestRunner.java
+++ src/test/java/com/petstore/runners/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com.petstore.steps",
+        glue = {"com.petstore.steps", "com.petstore.hooks"},
         plugin = {"pretty", "html:target/cucumber-reports.html"},
         monochrome = true
 )
